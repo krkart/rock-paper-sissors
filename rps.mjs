fba5f203f47840cab11400c24d9c04412d7042ff
krkart <krkart@zohomail.in>
2024-04-25 19:18:44 +0530

    loop added to track rounds

--- rps.mjs
+++ rps.mjs
@@ -4,24 +4,28 @@
 
 import readline from 'readline';
 
-function promptUser(query) {
-  const rl = readline.createInterface(process.stdin, process.stdout);
+for(let round=1; round <= 5; round++) {
+	console.log("\nRound: "+ round);
 
-  return new Promise(resolve => rl.question(query, userInput => {
-    rl.close();
-    resolve(userInput);
-  }))
+	function promptUser(query) {
+	  const rl = readline.createInterface(process.stdin, process.stdout);
+
+	  return new Promise(resolve => rl.question(query, userInput => {
+	    rl.close();
+	    resolve(userInput);
+	  }))
+	}
+
+	let userChoice = await promptUser("Enter your choice ( rock | paper | sissor ) : ");
+
+	const choiceArr = ["rock", "paper", "sissor"];
+
+	function randNum(min, max) {
+	  return Math.floor(Math.random() * (max - min) + min);
+	}
+
+	let randInt = randNum(0, choiceArr.length - 1);
+	let computerChoice = choiceArr[randInt];
+
+	console.log("your choice is " + userChoice + " & computer's choice is " + computerChoice);
 }
-
-let userChoice = await promptUser("Enter your choice ( rock | paper | sissor ) : ");
-
-const choiceArr = ["rock", "paper", "sissor"];
-
-function randNum(min, max) {
-  return Math.floor(Math.random() * (max - min) + min);
-}
-
-let randInt = randNum(0, choiceArr.length - 1);
-let computerChoice = choiceArr[randInt];
-
-console.log("your choice is " + userChoice + " & computer's choice is " + computerChoice);
