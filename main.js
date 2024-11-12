b486081ff679b8171c0ae3a5720469508bbff324
krkart <krkart@zohomail.in>
2024-11-13 00:33:21 +0530

    game & round logic complete with UI tweaks

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 let userScore = 0;
 let computerScore = 0;
+let tieCount = 0;
 let roundCount = 3;
 
 let userChoice = document.querySelector('.userChoice');
@@ -9,14 +10,15 @@ let compImg = document.querySelector('.compImg');
 let result = document.querySelector('.result');
 let score = document.querySelector('.score');
 let rounds = document.querySelectorAll(".rounds button");
+let pickBtn = document.querySelectorAll(".choices button");
 
 rounds.forEach((round) => {
-	round.addEventListener('click', () => {
-			roundCount = parseInt(round.value)
+	round.addEventListener('click', (e) => {
+		roundCount = parseInt(e.target.value)
 	})
 })
 
-function choice(e) {
+function getCompChoice(){
 	const choiceArr = ["rock", "paper", "sissor"];
 
 	function randNum(min, max) {
@@ -24,34 +26,70 @@ function choice(e) {
 	}
 
 	let randInt = randNum(0, choiceArr.length);
-	let computerChoice = choiceArr[randInt];
-
-		// Best practice with explanation, Ref: https://stackoverflow.com/a/56909452/6028958
-		if (e.target.classList.contains("chosen-btn")) {
-
-			if (e.target.value === computerChoice) {
-				result.textContent = "Both choices are same. No score counts for this round";
-			} else if (((e.target.value === "rock") && (computerChoice === "paper")) ||
-				((e.target.value === "paper") && (computerChoice === "sissor")) ||
-				((e.target.value === "sissor") && (computerChoice === "rock"))) {
-				result.textContent = "Computer Won";
-			} else {
-				result.textContent = "You Won";
-			}
-
-			userImg.innerHTML = `<img src="./img/${e.target.value}.png">`;
-			userChoice.textContent = e.target.value;
-			compImg.innerHTML = `<img src="./img/${computerChoice}.png">`;
-			compChoice.textContent = computerChoice;
-
-			if (result.textContent === "Computer Won") {
-				computerScore++
-			} else if (result.textContent === "You Won") {
-				userScore++
-			}
-
-			score.innerHTML = "<p>" + "Your Score = " + userScore + ". Computer Score = " + computerScore + "." + "</p>"
-		}
+  return choiceArr[randInt];
 }
 
-document.addEventListener('click', choice);
+let gameOver = false
+
+function gameLogic(humanChoice, computerChoice) {
+
+	// Best practice with explanation, Ref: https://stackoverflow.com/a/56909452/6028958
+	// if (e.target.classList.contains("pickBtn")) {
+
+		if (humanChoice === computerChoice) {
+			result.textContent = "Both choices are same. No score counts for this round";
+			tieCount++
+		} else if (((humanChoice === "rock") && (computerChoice === "paper")) ||
+			((humanChoice === "paper") && (computerChoice === "sissor")) ||
+			((humanChoice === "sissor") && (computerChoice === "rock"))) {
+			result.textContent = "Computer Won";
+			computerScore++
+		} else {
+			result.textContent = "You Won";
+			userScore++
+		}
+
+		userImg.innerHTML = `<img src="./img/${humanChoice}.png">`;
+		userChoice.textContent = humanChoice;
+		compImg.innerHTML = `<img src="./img/${computerChoice}.png">`;
+		compChoice.textContent = computerChoice;
+
+		score.innerHTML = "<p>" + "Your Score = " + userScore + ". Computer Score = " + computerScore + "." + "</p>"
+
+		if (computerScore === Math.ceil(((roundCount + 1) - tieCount) / 2)) {
+			const computerWin = document.createElement("h3");
+			computerWin.classList.add("winner")
+			computerWin.textContent = `The computer won with a ${computerScore - userScore} point lead!`
+			score.appendChild(computerWin)
+			gameOver = true
+
+		} else if (userScore === Math.ceil(((roundCount + 1) - tieCount) / 2)) {
+			const userWin = document.createElement("h3");
+			userWin.classList.add("winner")
+			userWin.textContent = `You won with a ${userScore - computerScore} point lead!`
+			score.appendChild(userWin)
+			gameOver = true
+		}
+	// }
+}
+
+pickBtn.forEach((button) => {
+	button.addEventListener('click', (e) =>{
+		if(gameOver) {
+			userScore = 0;
+			computerScore = 0;
+			tieCount = 0;
+			userImg.innerHTML = "";
+		  userChoice.textContent = '';
+			compImg.innerHTML = "";
+		  compChoice.textContent = '';	
+			score.innerHTML = "";
+			gameOver = false;		// reset the flag
+			return;
+		}
+
+		let humanChoice = e.target.value;
+		let computerChoice = getCompChoice();
+		gameLogic(humanChoice, computerChoice);
+	})
+})
