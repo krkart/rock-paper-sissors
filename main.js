d000fabfc362caca59b3d13106d81c3fe1c42806
krkart <krkart@zohomail.in>
2024-11-12 21:03:38 +0530

    icons added

--- main.js
+++ main.js
@@ -1,16 +1,20 @@
 let userScore = 0;
 let computerScore = 0;
+let roundCount = 3;
 
 let userChoice = document.querySelector('.userChoice');
 let compChoice = document.querySelector('.compChoice');
+let userImg = document.querySelector('.userImg');
+let compImg = document.querySelector('.compImg');
 let result = document.querySelector('.result');
 let score = document.querySelector('.score');
-let rounds = document.querySelector(".rounds");
-let play = document.querySelector(".play-btn");
+let rounds = document.querySelectorAll(".rounds button");
 
-rounds.onclick = function () {
-	rounds = rounds.options[rounds.selectedIndex].value;
-}
+rounds.forEach((round) => {
+	round.addEventListener('click', () => {
+			roundCount = parseInt(round.value)
+	})
+})
 
 function choice(e) {
 	const choiceArr = ["rock", "paper", "sissor"];
@@ -22,35 +26,32 @@ function choice(e) {
 	let randInt = randNum(0, choiceArr.length);
 	let computerChoice = choiceArr[randInt];
 
-	// Best practice with explanation, Ref: https://stackoverflow.com/a/56909452/6028958
-	if (e.target.classList.contains("chosen-btn")) {
+		// Best practice with explanation, Ref: https://stackoverflow.com/a/56909452/6028958
+		if (e.target.classList.contains("chosen-btn")) {
 
-		if (e.target.value === computerChoice) {
-			return "Both choices are same. No score counts for this round"
-		} else if (((e.target.value === "rock") && (computerChoice === "paper")) ||
-			((e.target.value === "paper") && (computerChoice === "sissor")) ||
-			((e.target.value === "sissor") && (computerChoice === "rock"))) {
-			result.textContent = "Computer Won";
-		} else {
-			result.textContent = "You Won";
+			if (e.target.value === computerChoice) {
+				result.textContent = "Both choices are same. No score counts for this round";
+			} else if (((e.target.value === "rock") && (computerChoice === "paper")) ||
+				((e.target.value === "paper") && (computerChoice === "sissor")) ||
+				((e.target.value === "sissor") && (computerChoice === "rock"))) {
+				result.textContent = "Computer Won";
+			} else {
+				result.textContent = "You Won";
+			}
+
+			userImg.innerHTML = `<img src="./img/${e.target.value}.png">`;
+			userChoice.textContent = e.target.value;
+			compImg.innerHTML = `<img src="./img/${computerChoice}.png">`;
+			compChoice.textContent = computerChoice;
+
+			if (result.textContent === "Computer Won") {
+				computerScore++
+			} else if (result.textContent === "You Won") {
+				userScore++
+			}
+
+			score.innerHTML = "<p>" + "Your Score = " + userScore + ". Computer Score = " + computerScore + "." + "</p>"
 		}
-		userChoice.textContent = e.target.value;
-		compChoice.textContent = computerChoice;
-
-		if (result.textContent === "Computer Won") {
-			computerScore++
-		} else if (result.textContent === "You Won") {
-			userScore++
-		}
-
-		score.innerHTML = "<p>" + "Your Score = " + userScore + ". Computer Score = " + computerScore + "." + "</p>"
-	}
 }
 
-play.addEventListener('click', playRounds);
-
-function playRounds(e) {
-	for (let round = 1; round <= rounds; round++) {
-		document.addEventListener('click', choice);
-	}
-}
+document.addEventListener('click', choice);
